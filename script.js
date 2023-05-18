5e9ad15d4db342602ed448d1ff29f0c2d3e48784
Luke-TS <132500533+Luke-TS@users.noreply.github.com>
2023-05-18 14:33:09 -0500

    selections are validated before stored

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const board = document.getElementById('board');
 
+let availableSelections = createArray(3, 3, 1);
 let p1Data = createArray(3, 3, 0);
 let p2Data = createArray(3, 3, 0);
-let currentTurn = 'p1'; // indicates player turn
+let p1TurnBool = true; // indicates player turn
 
 function makeGrid(dim) {
     for(let row = 0; row < dim; row++) {
-        let container = document.createElement('div');
+        const container = document.createElement('div');
 
         for(let col = 0; col < dim; col++) {
             const square = document.createElement('button');
             square.addEventListener('click', () => {
-                storeData(row, col, currentTurn);
-                switchTurn();
+                const valid = validateChoice(row, col);
+                if (valid) {
+                    storeData(row, col);
+                    switchTurn();
+                    displayData();
+                }
             })
             container.appendChild(square);
         }
@@ -20,31 +25,25 @@ function makeGrid(dim) {
     }
 }
 
-function storeData(r, c, turn) {
-    switch(turn) {
-        case 'p1':
-            p1Data[r][c] = 1;
-            break;
-        case 'p2':
-            p2Data[r][c] = 1;
-            break;
-        default:
-            console.log('error');
-            break;
+function validateChoice(r, c) {
+    if (p1TurnBool) {
+        return p1Data[r][c] != availableSelections[r][c];
+    } else {
+        return p2Data[r][c] != availableSelections[r][c];
     }
 }
 
-function switchTurn() {
-    switch(currentTurn) {
-        case 'p1':
-            currentTurn = 'p2';
-            break;
-        case 'p2':
-            currentTurn = 'p1';
-            break;
-        default:
-            console.log('error');
+function storeData(r, c) {
+    if (p1TurnBool) {
+        p1Data[r][c] = 1;
+    } else {
+        p2Data[r][c] = 1;
     }
+    availableSelections[r][c] = 0;
+}
+
+function switchTurn() {
+    p1TurnBool = !p1TurnBool;
 }
 
 function createArray(rows, cols, fillVal) {
@@ -53,6 +52,12 @@ function createArray(rows, cols, fillVal) {
 
 function startGame() {
     makeGrid(3);
+}
+
+function displayData() {
+    console.table(p1Data);
+    console.table(p2Data);
+    console.table(availableSelections);
 }
 
 startGame();
