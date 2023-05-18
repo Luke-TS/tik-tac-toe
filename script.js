9eb8079715de216c7544b69f5e281a1bdec05e2a
Luke-TS <132500533+Luke-TS@users.noreply.github.com>
2023-05-18 14:17:56 -0500

    user storage complete
    each player has 3x3 matrix of 0
    upon selection, a 1 is added

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
 const board = document.getElementById('board');
 
+let p1Data = createArray(3, 3, 0);
+let p2Data = createArray(3, 3, 0);
+let currentTurn = 'p1'; // indicates player turn
+
 function makeGrid(dim) {
-    for(let i = 0; i < dim; i++) {
+    for(let row = 0; row < dim; row++) {
         let container = document.createElement('div');
-        container.classList.add = 'container';
 
-        for(let j = 0; j < dim; j++) {
-            const square = {
-                btn: document.createElement('button'),
-            };
-            square.btn.classList.add = 'square';
-            square.btn.addEventListener('mouseover', () => {
-
-            });
-            container.appendChild(square.btn);
+        for(let col = 0; col < dim; col++) {
+            const square = document.createElement('button');
+            square.addEventListener('click', () => {
+                storeData(row, col, currentTurn);
+                switchTurn();
+            })
+            container.appendChild(square);
         }
         board.appendChild(container);
     }
 }
 
-makeGrid(3);
+function storeData(r, c, turn) {
+    switch(turn) {
+        case 'p1':
+            p1Data[r][c] = 1;
+            break;
+        case 'p2':
+            p2Data[r][c] = 1;
+            break;
+        default:
+            console.log('error');
+            break;
+    }
+}
+
+function switchTurn() {
+    switch(currentTurn) {
+        case 'p1':
+            currentTurn = 'p2';
+            break;
+        case 'p2':
+            currentTurn = 'p1';
+            break;
+        default:
+            console.log('error');
+    }
+}
+
+function createArray(rows, cols, fillVal) {
+    return Array(rows).fill().map(() => Array(cols).fill(fillVal));
+}
+
+function startGame() {
+    makeGrid(3);
+}
+
+startGame();
